ab07f9d77b37d3a60a05dd71d98f104da901dd0a
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-03 02:17:06 +0200

    Add User Not Complete

--- cafe/config/routes.rb
+++ cafe/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   devise_for :users
-  root :to =>'users#index'
+  resources :users
+  root :to =>'users#signin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
