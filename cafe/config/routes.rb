961403ea0593182d65ee9784ef7e8a8ee9549648
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-03 18:31:29 +0200

    Enable Only The Admin To Add Users

--- cafe/config/routes.rb
+++ cafe/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :users
   resources :users
-  root :to =>'users#signin'
+  root :to =>'orders#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
