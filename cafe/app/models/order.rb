af2ad2bee15d6855f5ff55593f78088973f98dda
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-05 18:32:24 +0200

    Set Default Status For The Order As Delivered To Cafe

--- cafe/app/models/order.rb
+++ cafe/app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :orders_products,dependent: :destroy
   has_many :products , through:   :orders_products
+  before_save :default_values
+  private
+  def default_values
+    self.status ||= "Delivered"
+  end
 end
