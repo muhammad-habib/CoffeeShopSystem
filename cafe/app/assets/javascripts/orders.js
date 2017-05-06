e7fd316fb35498b389b6bc3dfc9230dad58a59fc
seifElislam <seif.eleslam1990@gmail.com>
2017-05-06 23:27:15 +0200

    fix bug in change order state

--- cafe/app/assets/javascripts/orders.js
+++ cafe/app/assets/javascripts/orders.js
@@ -2,6 +2,7 @@
  * Created by seif on 5/3/17.
  */
 $(document).ready(function () {
+
     $(".changeStatus").change(function () {
         $.ajax({
             url: "/changeStatus",
@@ -39,4 +40,15 @@ $(document).ready(function () {
             //
             // })
     });
+    $(document).on("change",".changeStatus",function () {
+        $.ajax({
+            url: "/changeStatus",
+            data: {
+                user: $(this).data('user'),
+                order: $(this).data('id'),
+                status: $(this).val()
+            },
+            method: 'GET'
+        })
+    })
 });
