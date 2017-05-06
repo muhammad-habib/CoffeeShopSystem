8fd6f2832495efb1ff7061f773f35378e6d26a4b
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-06 14:44:47 +0200

    Adjust UI

--- cafe/app/models/order.rb
+++ cafe/app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :orders_products,dependent: :destroy
   has_many :products , through:   :orders_products
-  
+
   # before_save :default_values
   # private
   # def default_values
