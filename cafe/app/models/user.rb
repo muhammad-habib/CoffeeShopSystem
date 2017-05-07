1f301f44a296a8ee88e9d552732130850c596a47
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-07 13:03:43 +0200

    Fix Bug

--- cafe/app/models/user.rb
+++ cafe/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders
+  has_many :orders ,dependent: :destroy
   mount_uploader :image, ImageUploader
 end
